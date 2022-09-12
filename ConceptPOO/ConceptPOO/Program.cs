70fed0e0880977106a9ca50ec91eae9e6c94c14c
araigeorge <araigeorge@gmail.com>
2022-09-12 16:10:06 -0400

    validacion de dias

---
+++ ConceptPOO/ConceptPOO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConceptPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POO Concept!");
+            Console.WriteLine("============");
+
+            try
+            {
+                Console.WriteLine(new Date(2024, 2, 29));
+                Console.WriteLine(new Date(2021, 12, 21));
+                Console.WriteLine(new Date(2022, 11, 31));
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine(error.Message);
+            }
+
+        }
+    }
+}
